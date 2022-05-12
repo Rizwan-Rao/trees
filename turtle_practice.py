0f5e65c242d014559fe1335ef81c70d0b22edd43
Rizwan-Rao <67910809+Rizwan-Rao@users.noreply.github.com>
2022-05-12 13:33:43 +0530

    msg

---
+++ turtle_practice.py
@@ -0,0 +1,49 @@
+import turtle
+s= turtle.getscreen()
+t= turtle.Turtle()
+turtle.title("My turle program")
+# t.right(90)
+# t.forward(100)
+# t.left(90)
+# t.backward(100)
+# t.goto(100,500)
+# t.home()
+
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.circle(50)
+# turtle.bgcolor("blue")
+
+
+# t.begin_fill()
+# t.fd(100)
+# t.lt(120)
+# t.fd(100)
+# t.lt(120)
+# t.fd(100)
+# t.end_fill()
+
+
+# t.pen(pencolor="purple", fillcolor="orange", pensize=10, speed=9)
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+
+
+# c= t.clone()
+# t.color("magenta")
+# c.color("green")
+# t.circle(60)
+# c.circle(30)
+
+
+n=0
+while n <=50:
+  t.circle(n)
+  n+=10
